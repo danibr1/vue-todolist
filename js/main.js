7a02830c2cfb68a29a728f546a9896bea57b4008
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-04-22 20:04:20 +0200

    completed

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@
 const app = new Vue({
     el: '#app',
     data: {
+
+        // DEFAUL TODOLIST
         todos: [
             {
                 text: 'Chimare avv. Salvini per pratica 1213',
@@ -18,6 +20,34 @@ const app = new Vue({
                 text: 'Firmare bilancio',
                 completed: false,
             },
-        ]
+        ],
+        newTodo: '',
+    },
+    methods: {
+
+        // INSERT A NEW TODO
+        addTodo(){
+            if(this.newTodo !== ''){
+
+                this.todos.push({
+                    text: this.newTodo,
+                    completed: false,
+                });
+
+                this.newTodo = '';
+                this.$refs.todoInput.focus();
+            }
+        },
+
+         // REMOVE TODO
+        removeTodo(index) {
+            this.todos.splice(index, 1);
+        },
+
+        // UPDATE TODOS
+        updateStatus(index) {
+            this.todos[index].completed = ! this.todos[index],
+            completed;
+        }
     }
 });
